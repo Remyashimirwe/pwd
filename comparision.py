5587de7cb3c91461d3ff3dd8d246ca9a86880001
Recky <remyashimirwe48@gmail.com>
2026-01-29 20:05:11 +0200

    teaching about this

---
+++ comparision.py
@@ -0,0 +1,22 @@
+y = 20
+x= 10
+age = 17
+id = 65
+print(x != y)
+if not(age < 18 and id < 60):
+      print("Eligible for voting")
+else:
+      print("Not eligible for voting")
+a= 10
+b= 20
+b %= a
+print(b)
+fruits = ["apple", "banana"]
+print("egg" not in fruits)  
+name = input("Enter your name: ")
+school_students = ["remy", "john", "doe"]
+for student in school_students:
+    if age > 18:
+      print("Welcome back to the school!")
+    elif name in school_students:
+      print("You allowed to enter the school.")
